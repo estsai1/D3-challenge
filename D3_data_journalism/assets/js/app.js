5a71f05ea8fb22291cc9304c0303c069f80ff66d
freu <et9419@yahoo.com>
2020-08-20 05:12:43 -0400

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,15 +1,15 @@
 // @TODO: YOUR CODE HERE!
 
 // Define SVG area dimensions
-var svgWidth = 600;
-var svgHeight = 400;
+var svgWidth = 960;
+var svgHeight = 500;
 
 // Define the chart's margins as an object
 var chartMargin = {
-    top: 10,
-    right: 10,
-    bottom: 10,
-    left: 10
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
 };
 
 // Define dimensions of the chart area
@@ -48,7 +48,7 @@ d3.csv("https://raw.githubusercontent.com/estsai1/test/master/data.csv").then(fu
 
     // Set up bottom axis
     var x_scale = d3.scaleLinear()
-        .domain([0, d3.max(data, entry => entry.poverty)])
+        .domain([8, d3.max(data, entry => entry.poverty)])
         .range([0, chartWidth]);
     var bottom_axis = d3.axisBottom(x_scale);
     chartGroup.append("g")
@@ -58,14 +58,52 @@ d3.csv("https://raw.githubusercontent.com/estsai1/test/master/data.csv").then(fu
 
     // Set up left axis
     var y_scale = d3.scaleLinear()
-        .domain([0, d3.max(data, entry => entry.healthcare)])
+        .domain([4, d3.max(data, entry => entry.healthcare)])
         .range([chartHeight, 0]);
     var left_axis = d3.axisLeft(y_scale);
     chartGroup.append("g")
         .classed("axis", true)
         .call(left_axis);
 
+    // Create axis labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - chartMargin.left + 40)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");        
+
     // Draw states
-    
+    var circles_group = chartGroup.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", data => x_scale(data.poverty))
+        .attr("cy", data => y_scale(data.healthcare))
+        .attr("r", 15)
+        .attr("fill", "skyblue");
+
+    // Add state abbreviations to circles
+    // Doesn't work quite right
+    var state_names = chartGroup.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("dx", function(data) {
+            return x_scale(data.poverty) - 12;
+        })
+        .attr("dy", function(data) {
+            return y_scale(data.healthcare) + 5;
+        })
+        .style("fill", "white")
+        .text(function (data) {
+            return data.abbr;
+        });
 
 });
